79d2543adc49fd092fc937dd43fefedcc641881b
davonxo <210656033+davonxo@users.noreply.github.com>
2025-05-23 15:20:41 -0400

    Add index.html main.js style.css Edit package.json

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // We'll add this later if needed
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
